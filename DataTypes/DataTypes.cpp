ef6183ccb42c695c77107e073cdd8950cb6d1002
Linkknight <jawatson@student.neumont.edu>
2021-04-04 22:35:41 -0600

    Final Build

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
-
 using namespace std;
-float TAX = 0.1f;
+
+const float TAX = 0.1f;
 float totalHours = 0.0;
+string name;
+string initial;
+int age;
+bool isAdult;
+int zipCode;
+float wage;
+int daysWorked;
+float hoursWorkedPerDay[7];
 
 
 int main() {
-	string name;
-	string initial;
-	int age;
-	bool isAdult;
-	int zipCode;
-	float wage;
-	float grossIncome = totalHours * wage;
-	float netIncome = grossIncome - TAX;
-	int daysWorked;
-	float hoursWorkedPerDay[7];
-
-	cout << "Enter your first name please: ";
+	std::cout << "Enter your first name please: ";
 	cin >> name;
 	
-	cout << "Please enter your initials: ";
+	std::cout << "Please enter your initials: ";
 	cin >> initial;
 
-	cout << "How old are you: ";
+	std::cout << "How old are you: ";
 	cin >> age;
 
 	if (age >= 18) {
@@ -31,29 +28,26 @@ int main() {
 		cout << "You are over 21, you are an Adult.";
 	}
 	else isAdult = false;
-	cout << "You are not over 21. How did you get here?";
+	std::cout << "You are not over 21. How did you get here?";
 
-	cout << "What is your Zip Code: ";
+	std::cout << "What is your Zip Code: ";
 	cin >> zipCode;
 
-	cout << "What is your wage: ";
-	cin >> wage;
-
-	cout << "How many days do you work: ";
+	std::cout << "How many days do you work a week: ";
 	cin >> daysWorked;
 
-	/*for (int i = 0; i < daysWorked; i++) {
-		cout << "Enter your hours worked for the day: ";
-		cin >> hoursWorkedPerDay;
-	}*/
+	for (int i = 0; i < daysWorked; i++) {
+		cout << "Enter your hours worked for the day" << i+1 << " : ";
+		cin >> hoursWorkedPerDay[i];
+		totalHours += hoursWorkedPerDay[i];
+	}
 
-	cout << "Name: " + name;
-	cout << "Initials: " + initial;
-	cout << "Age: " + age;
-	cout << "Zipcode: " + zipCode;
-	cout << "Wage: " + wage;
-	cout << "Days Worked: " + daysWorked;
-	cout << "Gross Income: " + grossIncome;
-	cout << "Net Income: " + netIncome;
+	std::cout << "What is your wage: ";
+	cin >> wage;
 
+	int grossIncome = totalHours * (float)wage;
+	int netIncome = grossIncome - TAX;
+
+	cout << grossIncome;
+	cout << netIncome;
 }
